5baf6816f60707a55d359dd80fac05b5838788ad
Felipe Gamboa <feligamboaga@gmail.com>
2022-10-26 18:55:37 -0500

    primera version con clase de personas

---
+++ git/pruebaGIT.py
@@ -0,0 +1,17 @@
+class personas():
+    def __init__(self, nombre, peso, altura, pais_de_origen) -> None:
+        self.name = nombre
+        self.weight = peso
+        self.height = altura
+        self.nationality = pais_de_origen
+
+
+
+
+
+
+Felipe = personas('Felipe', 70, 179, "Colombia")   
+print(Felipe.nationality)
+print(Felipe.height)
+print(Felipe.name)
+    
