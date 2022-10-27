b0cd608c5477fefb83789f723fdcf35b0d55b13b
Felipe Gamboa <feligamboaga@gmail.com>
2022-10-26 19:18:29 -0500

    nueva funcion bmi

--- git/pruebaGIT.py
+++ git/pruebaGIT.py
@@ -4,14 +4,17 @@ class personas():
         self.weight = peso
         self.height = altura
         self.nationality = pais_de_origen
+    
+    def bmi(self):
+        bmi = self.weight / (self.height ** 2)
+        print(bmi)
+        
 
 
-
-
-
-
-Felipe = personas('Felipe', 70, 179, "Colombia")   
+Felipe = personas('Felipe', 70, 1.79, "Colombia")   
 print(Felipe.nationality)
 print(Felipe.height)
 print(Felipe.name)
+print(Felipe.bmi())
+
     
